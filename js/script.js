06374630e2331dfa5278d5bd1c774b32981dcf73
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-09-21 09:15:59 -0400

    added more bootstrap

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(function () {
     });
     $("#carouselButton").click(function () { });
     
-    
+
 });
 
 
@@ -63,3 +63,60 @@ $(function () {
         // console.log(typeof weather.main[0])
     }
 });
+
+// get picture of the day 
+// $(function () {
+//   // const Variables
+
+//   const BASE_URL = "https://images-api.nasa.gov/search?q=";
+
+//   let nasaData;
+
+//   // cached Elements
+//   const $title = $("#title");
+//   const $description = $("#description");
+//   const $picture = $("#picture");
+//   const $form = $("form");
+//   const $input = $('input[type="text"]');
+
+//   // Event listners
+//   $form.on("submit", handleGetData);
+
+//   // function deleteData() {
+
+//   // }
+
+//   // functions
+//   function handleGetData(event) {
+//     event.preventDefault();
+//     const searchText = $input.val();
+//     $input.val("");
+
+//     // get api
+//     $.ajax(`${BASE_URL}${searchText}&media_type=image&keywords=space`).then(
+//       function (data) {
+//         console.log(data);
+//         nasaData = data;
+//         render();
+//       },
+
+//       // error function
+
+//       function (error) {
+//         // the failure callback
+//         console.log(error);
+//         alert("ERROR 404");
+//       }
+//     );
+//   }
+
+//   // render  function
+//   function render() {
+//     $title.text(nasaData.collection.items[1].data[0].title);
+//     $description.text(nasaData.collection.items[1].data[0].description);
+//     $picture.html(`<img src="${nasaData.collection.items[1].links[0].href}">`);
+
+//     //   console.log(JSON.stringify())
+//     // console.log(typeof weather.main[0])
+//   }
+// });
