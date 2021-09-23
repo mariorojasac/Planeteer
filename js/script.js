6b391777042240489159e8077912eb790316f36d
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-09-22 21:10:40 -0400

    added modal fixed bugs and refactored code

--- js/script.js
+++ js/script.js
@@ -1,65 +1,67 @@
 $(function () {
-    $(".carousel").carousel({
-        interval: 3000
-    });
-    $("#carouselButton").click(function () { });
-    
+  $(".carousel").carousel({
+    interval: 3000
+  });
+  $("#carouselButton").click(function () {});
+
 
 });
 
 
 $(function () {
-    // const Variables
+  // const Variables
 
-    const BASE_URL = "https://images-api.nasa.gov/search?q=";
-    //   const API_KEY = "79p2b1vovVqtaHcgHNcWki4H9SKLN4DeB1NNGNvh";
-    let nasaData;
+  const BASE_URL = "https://images-api.nasa.gov/search?q=";
+  //   const API_KEY = "79p2b1vovVqtaHcgHNcWki4H9SKLN4DeB1NNGNvh";
+  let nasaData;
 
-    // cached Elements
-    const $title = $("#title");
-    const $description = $("#description");
-    const $picture = $("#picture");
-    const $form = $("form");
-    const $input = $('input[type="text"]');
+  // cached Elements
+  const $title = $("#title");
+  const $description = $("#description");
+  const $dateof = $("#dateof")
+  const $picture = $("#picture");
+  const $form = $("form");
+  const $input = $('input[type="text"]');
 
-    // Event listners
-    $form.on("submit", handleGetData);
-
-  
-
-    // functions
-    function handleGetData(event) {
-        event.preventDefault();
-        const searchText = $input.val();
-        $input.val("");
-
-        // get api
-        $.ajax(`${BASE_URL}${searchText}&media_type=image&keywords=space`).then(function (data) {
-                console.log(data);
-                nasaData = data;
-                render();
-            },
-
-            // error function
-
-            function (error) {
-             
-                console.log(error);
-                alert("ERROR 404");
-            }
-        );
-    }
-
-    // render  function
-    function render() {
-        $title.text(nasaData.collection.items[1].data[0].title);
-        $description.text(nasaData.collection.items[1].data[0].description);
-        $picture.html(`<img src="${nasaData.collection.items[1].links[0].href}">`);
+  // Event listners
+  $form.on("submit", handleGetData);
 
 
-        //   console.log(JSON.stringify())
-        // console.log(typeof weather.main[0])
-    }
+
+  // functions
+  function handleGetData(event) {
+    event.preventDefault();
+    const searchText = $input.val();
+    $input.val("");
+
+    // get api
+    $.ajax(`${BASE_URL}${searchText}&media_type=image&keywords=space`).then(function (data) {
+        console.log(data);
+        nasaData = data;
+        render();
+      },
+
+      // error function
+
+      function (error) {
+
+        console.log(error);
+        alert("ERROR 404");
+      }
+    );
+  }
+
+  // render  function
+  function render() {
+    $title.text(nasaData.collection.items[1].data[0].title);
+    $dateof.text(nasaData.collection.items[1].data[0].date_created)
+    $description.text(nasaData.collection.items[1].data[0].description);
+    $picture.html(`<img src="${nasaData.collection.items[1].links[0].href}">`);
+
+
+    //   console.log(JSON.stringify())
+    // console.log(typeof weather.main[0])
+  }
 });
 
 
@@ -70,7 +72,7 @@ $(function () {
 $(function () {
   // const Variables
 
-  const BASE_URL = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+  const BASE_URL = "https://api.nasa.gov/planetary/apod?api_key=79p2b1vovVqtaHcgHNcWki4H9SKLN4DeB1NNGNvh";
 
   let nasaData;
 
@@ -78,18 +80,19 @@ $(function () {
   const $title = $("#titleofday");
   const $description = $("#descriptionofday");
   const $picture = $("#pictureofday");
+  const $video = $("#videoofday")
   const $button = $("#buttonoftheday");
 
 
   // Event listners
-  $("#car").on("click", handleGetData);
+  $button.on("click", handleGetData);
 
 
 
   // functions
   function handleGetData(event) {
     event.preventDefault();
-    
+
 
     // get api
     $.ajax(`${BASE_URL}`).then(
@@ -101,16 +104,21 @@ $(function () {
 
 
       function (error) {
-       
+
         console.log(error);
         alert("ERROR 404");
       }
     );
   }
+
   function render() {
     $title.text(`Picture Of The Day: ${nasaData.title}`);
     $description.text(nasaData.explanation);
-    $picture.html(`<img id="pictureoftheday" src="${nasaData.url}">`);
+    $picture.html(`<img class="d-block  img-fluid" id="pictureoftheday" src="${nasaData.url}">`);
+    $video.html(`<iframe width="200"
+      height="150"  id="pictureoftheday"  src="${nasaData.url}" title="YouTube video player" frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen >`);
 
   }
 });
